1dc7718ab41a164fe600626410e023021b765770
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 17:50:55 +0530

    Added: Rolling a die

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,8 +1,24 @@
 public class SnakeLadder {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int p1 = 0;
-        if (p1 == 0) {
+        if (p1 ==0) {
             System.out.println("Player is at " + p1 + " position.");
         }
+        int roll_die = (int) ((Math.random()*10) % 6);
+        if (roll_die == 1){
+            System.out.println("Outcome: "+ roll_die);
+        } else if (roll_die == 2) {
+            System.out.println("Outcome: "+ roll_die);
+        } else if (roll_die == 3) {
+            System.out.println("Outcome: "+ roll_die);
+        } else if (roll_die == 4) {
+            System.out.println("Outcome: "+ roll_die);
+        } else if (roll_die == 5) {
+            System.out.println("Outcome: "+ roll_die);
+        } else {
+            System.out.println("Outcome: "+ 6);
+        }
+        p1 += roll_die;
+        System.out.println("New Position: "+ p1);
     }
 }
