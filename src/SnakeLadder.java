cb5acb0d0e922b4ce920618c44c7105bfe71527e
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 17:45:26 +0530

    Added: Player at 0

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,5 +1,8 @@
 public class SnakeLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to snake and ladder");
+        int p1 = 0;
+        if (p1 == 0) {
+            System.out.println("Player is at " + p1 + " position.");
+        }
     }
 }
