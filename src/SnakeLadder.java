e01355d0fcc18fbbf2bf714fda3498c3efeb84ca
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 18:06:21 +0530

    Added: Player to reach 100

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -3,42 +3,37 @@ import java.util.*;
 public class SnakeLadder {
     public static void main(String[] args){
         int p1 = 0;
-        if (p1 ==0) {
-            System.out.println("Player is at " + p1 + " position.");
+        while (p1 != 100) {
+            int roll_die = (int) ((Math.random()*10) % 6);
+            if (roll_die == 0){
+                roll_die = 6;
+            }
+            if ((p1+roll_die) > 100){
+                continue;
+            }
+            p1 += roll_die;
+            String option = getRandomOption();
+            switch (option) {
+                case "No Play":
+                    break;
+                case "Ladder":
+                    if ((p1+roll_die) > 100) {
+                        break;
+                    }
+                    p1 += roll_die;
+                    break;
+                case "Snake":
+                    p1 -= roll_die;
+                    if (p1 < 0) {
+                        p1 = 0;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+            System.out.println("Outcome: "+ p1);
         }
-        int roll_die = (int) ((Math.random()*10) % 6);
-        if (roll_die == 1){
-            System.out.println("Outcome: "+ roll_die);
-        } else if (roll_die == 2) {
-            System.out.println("Outcome: "+ roll_die);
-        } else if (roll_die == 3) {
-            System.out.println("Outcome: "+ roll_die);
-        } else if (roll_die == 4) {
-            System.out.println("Outcome: "+ roll_die);
-        } else if (roll_die == 5) {
-            System.out.println("Outcome: "+ roll_die);
-        } else {
-            System.out.println("Outcome: "+ 6);
-        }
-        p1 += roll_die;
-        String option = getRandomOption();
-        switch (option) {
-            case "No Play":
-                break;
-            case "Ladder":
-                p1 += roll_die;
-                break;
-            case "Snake":
-                p1 -= roll_die;
-                if (p1 < 0) {
-                    p1 = 0;
-                }
-                break;
-            default:
-                System.out.println("Invalid option");
-        }
-
-        System.out.println("New Position: "+ p1);
+        System.out.println("It Reached at position "+ p1);
     }
     private static String getRandomOption() {
         String[] options = {"No Play", "Ladder", "Snake"};
